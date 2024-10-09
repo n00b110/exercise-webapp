8503a6b42316d503af75ac8c7fdfdaf1ebbac151
Immanuel Olaoye <olaoye939@gmail.com>
2024-10-09 11:11:57 -0500

    added bulma css

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import 'bulma/css/bulma.min.css';
+
 function App() {
 
   return (
     <>
-      
+      <div className="main">
+        <div className="buttons">
+          <button className="button is-primary">Hello World</button>
+          <button className="button is-success">I'm Successfully</button>
+        </div>
+      </div>
     </>
   )
 }
 
 export default App
+
+
