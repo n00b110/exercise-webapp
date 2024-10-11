6e46829ff4ee865ee3bd8ce111827346161cade8
Immanuel Olaoye <olaoye939@gmail.com>
2024-10-10 20:23:19 -0500

    added font-awesome

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 import 'bulma/css/bulma.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 function App() {
 
   return (
     <>
-      <div className="main">
-        <div className="buttons">
-          <button className="button is-primary">Hello World</button>
-          <button className="button is-success">I'm Successfully</button>
+      <div className="navbar">
+        <div className="navbar-brand">
+          <span className="icon-text">
+            <i className="fa-solid fa-dumbbell"></i>
+            <span>FitForYou</span>
+          </span>
         </div>
       </div>
     </>
@@ -20,5 +23,3 @@ function App() {
 }
 
 export default App
-
-
