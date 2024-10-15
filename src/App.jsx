8a87289db6bb8ec6ad6bed001dd0448534c7d05c
Immanuel Olaoye <olaoye939@gmail.com>
2024-10-14 19:18:14 -0500

    added placeholder for image swap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import "bulma/css/bulma.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 function App() {
+  // useEffect(() => {
+  //   const url = "https://v2.exercisedb.io/exercises?offset=0&limit=10"
+  //   fetch(url)
+  //   .then(response => response.json())
+  //   console.log(response.json())
+  // }, [])
+
   return (
     <>
       <nav className="navbar">
@@ -16,7 +21,7 @@ function App() {
           </span>
           <a
             role="button"
-            class="navbar-burger"
+            className="navbar-burger"
             aria-label="menu"
             aria-expanded="false"
             data-target="navbarBasicExample"
@@ -28,19 +33,32 @@ function App() {
           </a>
         </div>
         <div className="navbar-menu is-active">
-          <div className="navbar-start">
-            <a href="" className="navbar-item">
-              Workouts
-            </a>
-            <a href="" className="navbar-item">
-              Workouts
-            </a>
-          </div>
+          <div className="navbar-start"></div>
           <div className="navbar-end">
-
+            <div className="navbar-item">
+              <div className="buttons">
+                <button className="button is-danger">
+                  <span className="icon">
+                    <i class="fa-solid fa-recycle"></i>
+                  </span>
+                  <span>Get a random exercise</span>
+                </button>
+                <a href="" className="button is-primary">
+                  <span>Sign Up</span>
+                </a>
+                <a href="" className="button is-info">
+                  Login
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
+      <figure>
+        <img src="https://bulma.io/assets/images/placeholders/256x256.png" />
+        <img src="https://bulma.io/assets/images/placeholders/256x256.png" />
+        <figcaption>Figure 1: Some beautiful placeholders</figcaption>
+      </figure>
     </>
   );
 }
